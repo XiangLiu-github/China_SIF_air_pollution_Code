ec617ec8f860e5ea1e83869f70eb1658ba9d6377
XiangLiu-github <57359052+XiangLiu-github@users.noreply.github.com>
2022-11-19 16:50:40 +0800

    Update cal_prep.R

--- script/cal_prep.R
+++ script/cal_prep.R
@@ -21,27 +21,3 @@ prep <- prep %>%
 check_join(prep)
 
 saveRDS(prep, "data/outputs/prep/tidied.rds")
-
-
-# 1km ---------------------------------------------------------------------
-
-mask <- rast("data/outputs/masks/mask_extraction_1km.tif") %>% sum(na.rm = T)
-
-prep <-
-  rast("../data_archive/China_1km_climate/China_1km_prep_monsum.nc") %>%
-  `names<-`(time(.))
-
-walk(2000:2019, function(ayear) {
-  stopifnot(nlyr(prep[as.character(ayear)]) == 12)
-
-  prep[as.character(ayear)] %>%
-    resample(mask, method = "near") %>%
-    mask(mask) %>%
-    as.data.frame(xy = T) %>%
-    pivot_longer(-c(x, y),
-      names_to = "Date", names_transform = list(Date = ymd),
-      values_to = "prep"
-    ) %>%
-    mutate(year = year(Date), month = month(Date), .keep = "unused") %>%
-    qsave(str_c("data/outputs/prep/tidied_1km_", ayear, ".qs"), nthreads = 5)
-})
