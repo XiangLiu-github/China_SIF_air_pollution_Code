c8d75f063cf602c12029634fe18a342a4b4dc463
XiangLiu-github <57359052+XiangLiu-github@users.noreply.github.com>
2022-11-19 16:48:27 +0800

    Update get_imtigated_pollution.R

--- script/get_imtigated_pollution.R
+++ script/get_imtigated_pollution.R
@@ -43,44 +43,6 @@ aod_cft_all <-
 
 saveRDS(aod_cft_all, "data/aod_cft.rds")
 
-# aerosol2 -----------------------------------------------------------------
-
-pm <- read_rds("data/outputs/aerosol/PM25.rds")
-aod <- read_rds("data/outputs/aerosol/AOD.rds")
-
-joined <- inner_join(pm, aod)
-
-model <- feols(AOD ~ PM25 | x^y + month,
-  data = joined,
-  cluster = ~ x^y, nthreads = 0, combine.quick = F
-)
-
-esttable(model)
-
-month_climatology <- pm %>%
-  fgroup_by(x, y, month) %>%
-  fsummarise(PM25_month = fmean(PM25)) %>%
-  fgroup_by(x, y) %>%
-  fmutate(PM25_month_percent = PM25_month / fmean(PM25_month), .keep = "unused") %>%
-  fungroup()
-
-aod_cft_all <-
-  tibble(
-    pm_level = seq(0, 50, by = 5),
-    aod_data = map(pm_level, function(anum) {
-      month_climatology %>%
-        mutate(PM25 = anum * PM25_month_percent) %>%
-        mutate(
-          AOD_cft = predict(model, .),
-          AOD_cft = fifelse(AOD_cft < 0, 0, AOD_cft)
-        ) %>%
-        select(-starts_with("PM"))
-    })
-  )
-
-saveRDS(aod_cft_all, "data/aod_cft2.rds")
-
-
 # ozone -------------------------------------------------------------------
 
 ozone <- read_rds("data/outputs/ozone/tidied.rds") # unit is ug/m3
